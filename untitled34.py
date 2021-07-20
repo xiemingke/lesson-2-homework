27551e200075d853e891a270df24603fda2de6ae
xiemingke <87630939+xiemingke@users.noreply.github.com>
2021-07-20 17:04:49 +0800

    Add files via upload

---
+++ untitled34.py
@@ -0,0 +1,13 @@
+score=input("score?")
+
+if int(score)>int(90):
+    print("A")
+elif int (score)>int(80):
+    print("B")
+elif int(score)>int(70):
+    print("C")
+elif int (score)>int(60):
+    print("D")
+else:
+    print("F")
+
---
+++ untitled35.py
@@ -0,0 +1,12 @@
+math = input("math score")
+math = int(math)
+eng = input("english score")
+eng = int(eng)
+if math >= 90 and eng >=90:
+    print("有獎學金")
+elif math == 100 or eng == 100:
+    print("有獎學金") 
+else:
+    print("沒有獎學金") 
+    
+
